0ccb38c444671036caee00bc606e3ce70c82290d
Grace Rothwell <gracerothwell0306@gmail.com>
2026-04-09 22:28:07 -0400

    more jupyter debugging stuff

--- comm250_final/lung_cancer_manipulation.py
+++ comm250_final/lung_cancer_manipulation.py
@@ -1,3 +1,4 @@
+# %%
 import pandas as pd
 import numpy as np
 import seaborn as sns
@@ -15,12 +16,11 @@ label_encoder = preprocessing.LabelEncoder()
 # print(label_encoder.transform(df["Gender"]))
 for column in df.select_dtypes(include=["str"]):
     label_encoder.fit(df_copy[column])
-    print(list(label_encoder.classes_))
-    print(label_encoder.transform(df_copy[column]))
+    #print(list(label_encoder.classes_))
+    #print(label_encoder.transform(df_copy[column]))
     df_copy[column]=label_encoder.transform(df_copy[column])
 print(df_copy.head(), df.head())
 # %%
-survive_colors = {0:'black', 1:'blue'}
-pd.plotting.scatter_matrix(df_copy, figsize=(20, 20), grid=True, marker='o', c=df_copy['Survived'].map(survive_colors))
-sns.set_theme(style="ticks")
-sns.pairplot(df_copy, hue='Survived')
+pd.plotting.scatter_matrix(df['Age', 'Family_History', 'Survived'])
+plt.show()
+# %%
