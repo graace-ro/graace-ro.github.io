bef6cc1db1de5bee5c257003b5d3cfeea3c42900
Grace Rothwell <gracerothwell0306@gmail.com>
2026-04-09 22:11:33 -0400

    before restart

--- comm250_final/lung_cancer_manipulation.py
+++ comm250_final/lung_cancer_manipulation.py
@@ -8,6 +8,7 @@ df=df.drop(columns=["Diagnosis_Date", "Patient_ID"])
 df_copy = df.copy()
 print(df.head())
 print(df.info())
+# %%
 label_encoder = preprocessing.LabelEncoder()
 # label_encoder.fit(df["Gender"])
 # print(list(label_encoder.classes_))
@@ -18,3 +19,8 @@ for column in df.select_dtypes(include=["str"]):
     print(label_encoder.transform(df_copy[column]))
     df_copy[column]=label_encoder.transform(df_copy[column])
 print(df_copy.head(), df.head())
+# %%
+survive_colors = {0:'black', 1:'blue'}
+pd.plotting.scatter_matrix(df_copy, figsize=(20, 20), grid=True, marker='o', c=df_copy['Survived'].map(survive_colors))
+sns.set_theme(style="ticks")
+sns.pairplot(df_copy, hue='Survived')
