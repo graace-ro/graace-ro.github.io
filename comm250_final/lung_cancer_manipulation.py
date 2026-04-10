047b557357930446905fa744abb6bc971c7a544b
Grace Rothwell <gracerothwell0306@gmail.com>
2026-04-09 20:58:34 -0400

    trying to make categorical into numerical

--- comm250_final/lung_cancer_manipulation.py
+++ comm250_final/lung_cancer_manipulation.py
@@ -1,3 +1,13 @@
 import pandas as pd
+import numpy as np
+import seaborn as sns
+import matplotlib.pyplot as plt
+from sklearn import preprocessing
 df = pd.read_csv("./lung_cancer_data/lung_cancer_dataset.csv")
-print(df.head(1))
+df=df.drop(columns=["Diagnosis_Date", "Patient_ID"])
+print(df.head())
+print(df.info())
+label_encoder = preprocessing.LabelEncoder()
+label_encoder.fit(df["Gender"])
+print(list(label_encoder.classes_))
+print(label_encoder.transform(df["Gender"]))
