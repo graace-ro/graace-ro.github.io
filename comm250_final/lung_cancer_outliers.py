727a4d5923c10a71541a29ffe74a69ec159dec53
Grace Rothwell <gracerothwell0306@gmail.com>
2026-04-12 17:01:32 -0400

    outliers fixed and commented

--- comm250_final/lung_cancer_outliers.py
+++ comm250_final/lung_cancer_outliers.py
@@ -10,22 +10,22 @@ numeric_cols = df.select_dtypes(include='number').columns
 categorical_cols = df.select_dtypes(include="str").columns
 #print(numeric_cols)
 #print(categorical_cols)
-df['Numeric_Outlier'] = 'False'
-for group_value in ['No','Yes']:
-    group = df[df['Survived']==group_value]
-    for col in numeric_cols:
-        z = np.abs(stats.zscore(group[col]))
-        outlier_mask = (z>3)
-        df.loc[group.index[outlier_mask], 'Numeric_Outlier'] = True
+df['Numeric_Outlier'] = False #sets every entry in this new column to false
+for group_value in ['No','Yes']: #for the answers of no and yes
+    group = df[df['Survived']==group_value] #the group of this iteration is the group that is either yes or no
+    for col in numeric_cols: #for the numeric columns
+        z = np.abs(stats.zscore(group[col])) #get the z score of all the nos for that column and all the yes's for that column
+        outlier_mask = (z>3) #a boolean series of which values have a z score (deviation) that is greater than 3, which makes it an outlier
+        df.loc[group.index[outlier_mask], 'Numeric_Outlier'] = True #the indeces of these values, for that group, is set to true
 print(df.columns)
 
-df['Categorical_Outlier'] = 'False'
+df['Categorical_Outlier'] = False
 for group_value in ['No','Yes']:
     group = df[df['Survived']==group_value]
     for col in categorical_cols:
-        freq = group[col].value_counts(normalize=True)
-        rare_categories = freq[freq<0.05].index
-        group_mask = group[col].isin(rare_categories)
-        df.loc[group.index[group_mask], 'Categorical_Outlier']=True
-df['Is_Outlier'] = df['Categorical_Outlier'] | df['Numeric_Outlier']
+        freq = group[col].value_counts(normalize=True) #finding the frequency of the categorical variable
+        rare_categories = freq[freq<0.05].index #a rare category is one whose frequency is <0.05. we get the indeces of these rare categories
+        group_mask = group[col].isin(rare_categories) #making a mask like above for the rows that are rare categories
+        df.loc[group.index[group_mask], 'Categorical_Outlier']=True #set those rows to true
+df['Is_Outlier'] = df['Categorical_Outlier'] | df['Numeric_Outlier'] #combine the categorical and numerical
 print(df.head())
